478e2516f6e1f8af5f003534e4b9551df6c48bd6
shridharrai <shridharrai3@gmail.com>
2023-08-06 16:26:57 +0530

    feat(): navbar UI completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Experience from "@/components/Experience";
-import Tech from "@/components/Tech";
-import Works from "@/components/Works";
-import Feedbacks from "@/components/Feedbacks";
-import Contact from "@/components/Contact";
-import { StarsCanvas } from "@/components/canvas";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+import Feedbacks from "./components/Feedbacks";
+import Contact from "./components/Contact";
+import { StarsCanvas } from "./components/canvas";
 
 const App = () => {
   return (
