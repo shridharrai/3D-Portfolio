c9653c6332e4d8aa4f7fa2de3f36538b87b85393
shridharrai <shridharrai3@gmail.com>
2023-08-06 19:14:46 +0530

    feat(): one line intro section created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 inset-0`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-electric-purple" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
 
-export default Hero
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-electric-purple">Shridhar</span>
+          </h1>
+          <p className={`${styles.heroSubText} text-white-100 mt-2`}>
+            Innovative Software Developer, <br className="sm:block hidden" />
+            building web and mobile applications
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+    </section>
+  );
+};
+
+export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
+        "electric-purple": "#915eff",
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
