73bab4a63c1c61325f9308ba832845650c0a4833
shridharrai <shridharrai3@gmail.com>
2023-08-13 08:11:51 +0530

    feat(): added the animated button to scroll to about section from Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -25,6 +26,22 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-5 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
