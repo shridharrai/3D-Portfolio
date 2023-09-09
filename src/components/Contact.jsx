5656f4ae1536eda894fda1eeeead19c25f12e48b
shridharrai <shridharrai3@gmail.com>
2023-09-09 19:21:25 +0530

    feat(); added the modal

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,7 @@ import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import emailjs from "@emailjs/browser";
 import { personalInfo, publicUrls } from "../constants";
+import Modal from "./Modal";
 
 const Contact = () => {
   const formRef = useRef();
@@ -15,6 +16,13 @@ const Contact = () => {
     message: "",
   });
   const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [modalContent, setModalContent] = useState({
+    title: "",
+    message: "",
+    buttonText: "",
+  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -35,12 +43,18 @@ const Contact = () => {
           from_email: form.email,
           to_email: personalInfo.email,
           message: form.message,
+          reply_to: form.email,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
-          alert("Thank you. I will get back to you as soon as possilbe.");
+          setModalContent({
+            title: "Success!",
+            message: "Thank you. I will get back to you as soon as possilbe.",
+            buttonText: "Ok",
+          });
+          setIsModalVisible(true);
 
           setForm({
             name: "",
@@ -50,98 +64,114 @@ const Contact = () => {
         },
         (error) => {
           console.log("Error while sending mail ", error);
-
-          alert("Ahh, something went wrong. Please try again.");
+          setModalContent({
+            title: "Error!",
+            message: "Ahh, something went wrong. Please try again.",
+            buttonText: "Retry",
+          });
+          setIsError(true);
+          setIsModalVisible(true);
         }
       )
       .finally(() => setLoading(false));
   };
 
   return (
-    <div className="xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden">
-      <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className="relative flex-[0.75] bg-black-100 p-8 rounded-2xl"
-      >
-        <div className="flex items-center justify-end space-x-4 absolute top-8 right-4">
-          {Object.keys(publicUrls.socialProfiles).map((socialProfile) => {
-            const profile = publicUrls.socialProfiles[socialProfile];
-            return (
-              <div
-                key={`social_${profile.title}`}
-                onClick={() => window.open(profile.link, "_blank")}
-                className="green-pink-gradient lg:w-10 lg:h-10 h-8 w-8 rounded-full flex justify-center items-center cursor-pointer hover:scale-110"
-              >
-                <img
-                  src={profile.icon}
-                  alt={`social_${profile.title}`}
-                  className="w-4/6 h-4/6 object-contain"
-                />
-              </div>
-            );
-          })}
-        </div>
-
-        <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
-
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className="mt-12 flex flex-col gap-8"
+    <>
+      <div className="xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden">
+        <motion.div
+          variants={slideIn("left", "tween", 0.2, 1)}
+          className="relative flex-[0.75] bg-black-100 p-8 rounded-2xl"
         >
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className="bg-tertiary py-4 px-6 text-white placeholder:text-secondary rounded-lg outline-none border-none font-medium"
-            />
-          </label>
+          <div className="flex items-center justify-end space-x-4 absolute top-8 right-4">
+            {Object.keys(publicUrls.socialProfiles).map((socialProfile) => {
+              const profile = publicUrls.socialProfiles[socialProfile];
+              return (
+                <div
+                  key={`social_${profile.title}`}
+                  onClick={() => window.open(profile.link, "_blank")}
+                  className="green-pink-gradient lg:w-10 lg:h-10 h-8 w-8 rounded-full flex justify-center items-center cursor-pointer hover:scale-110"
+                >
+                  <img
+                    src={profile.icon}
+                    alt={`social_${profile.title}`}
+                    className="w-4/6 h-4/6 object-contain"
+                  />
+                </div>
+              );
+            })}
+          </div>
 
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Email</span>
-            <input
-              type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className="bg-tertiary py-4 px-6 text-white placeholder:text-secondary rounded-lg outline-none border-none font-medium"
-            />
-          </label>
+          <p className={styles.sectionSubText}>Get in touch</p>
+          <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Message</span>
-            <textarea
-              rows={7}
-              name="message"
-              value={form.message}
-              onChange={handleChange}
-              placeholder="What's you want to say?"
-              className="bg-tertiary py-4 px-6 text-white placeholder:text-secondary rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-
-          <button
-            type="submit"
-            className="bg-tertiary py-3 px-8 rounded-xl outline-none text-white font-bold w-fit shadow-md shadow-primary"
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="mt-12 flex flex-col gap-8"
           >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
-      </motion.div>
+            <label className="flex flex-col">
+              <span className="text-white font-medium mb-4">Your Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                placeholder="What's your good name?"
+                className="bg-tertiary py-4 px-6 text-white placeholder:text-secondary rounded-lg outline-none border-none font-medium"
+              />
+            </label>
 
-      <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
-        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
-      >
-        <EarthCanvas />
-      </motion.div>
-    </div>
+            <label className="flex flex-col">
+              <span className="text-white font-medium mb-4">Your Email</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                placeholder="What's your web address?"
+                className="bg-tertiary py-4 px-6 text-white placeholder:text-secondary rounded-lg outline-none border-none font-medium"
+              />
+            </label>
+
+            <label className="flex flex-col">
+              <span className="text-white font-medium mb-4">Your Message</span>
+              <textarea
+                rows={7}
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                placeholder="What's you want to say?"
+                className="bg-tertiary py-4 px-6 text-white placeholder:text-secondary rounded-lg outline-none border-none font-medium"
+              />
+            </label>
+
+            <button
+              type="submit"
+              className="bg-tertiary py-3 px-8 rounded-xl outline-none text-white font-bold w-fit shadow-md shadow-primary"
+            >
+              {loading ? "Sending..." : "Send"}
+            </button>
+          </form>
+        </motion.div>
+
+        <motion.div
+          variants={slideIn("right", "tween", 0.2, 1)}
+          className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+        >
+          <EarthCanvas />
+        </motion.div>
+      </div>
+      {isModalVisible && (
+        <Modal
+          title={modalContent.title}
+          message={modalContent.message}
+          buttonText={modalContent.buttonText}
+          isError={isError}
+          setIsModalVisible={() => setIsModalVisible(false)}
+        />
+      )}
+    </>
   );
 };
 
